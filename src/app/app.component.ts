643ca8b72ec26567cdf990ec4cd4b107d7931037
RafaelMQs <rafaeelmq08@gmail.com>
2022-06-10 19:14:35 -0300

    Iniciando projeto da Calculadora

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,14 +5,14 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  public appPages = [
-    { title: 'Inbox', url: '/folder/Inbox', icon: 'mail' },
-    { title: 'Outbox', url: '/folder/Outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/Favorites', icon: 'heart' },
-    { title: 'Archived', url: '/folder/Archived', icon: 'archive' },
-    { title: 'Trash', url: '/folder/Trash', icon: 'trash' },
-    { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
-  ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
+
   constructor() {}
+
+  onToggleColorTheme(event : any){
+    if(event.detail.checked){
+      document.body.setAttribute('color-theme', 'dark')
+    }else[
+      document.body.setAttribute('color-theme', 'ligth')
+    ]
+  }
 }
